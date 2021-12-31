65c23d5f185fbeedd74da779a4c284b73c514c05
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-12-31 15:26:37 +0000

    added cicd pipeline

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 
+app.get('/health', (req, res) => {
+  res.send('Healthy!')
+})
+
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
