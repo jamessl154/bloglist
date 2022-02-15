7cb6acb29869729f92e8e03ccf182e3540ebc3bd
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-02-15 15:06:40 +0000

    test workflow #skip

--- __tests__/user_api_test.js
+++ __tests__/user_api_test.js
@@ -10,10 +10,8 @@ describe('when there is initially one user in db', () => {
 
   beforeEach(async () => {
     await User.deleteMany({})
-
     const passwordHash = await bcrypt.hash('sekret', 10)
     const user = new User({ username: 'root', name: 'new test subject', passwordHash })
-
     await user.save()
   })
 
